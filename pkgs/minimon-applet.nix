6f2fb23611c3a458698e399cd6dc382d0b3e6660
Jeff Winget <jeff@3dgradebook.com>
2025-09-05 09:06:32 -0600

    Fixing the minimon applet

--- pkgs/minimon-applet.nix
+++ pkgs/minimon-applet.nix
@@ -1,25 +1,43 @@
 {
   lib,
-  rustPlatform,
   fetchFromGitHub,
-  pkg-config,
-  libxkbcommon,
+  libcosmicAppHook,
+  rustPlatform,
+  just,
+  stdenv,
+  nix-update-script,
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = "minimon-applet";
-  version = "0.8.0";
+  version = "unstable-2024-12-15";
 
   src = fetchFromGitHub {
     owner = "cosmic-utils";
     repo = "minimon-applet";
-    rev = "v${version}";
-    hash = "sha256-TK+H9HUgJZxVwcxhot+Jsbs7ZnBBtu5p5xXEahzs298=";
+    rev = "main"; # Use latest commit since tags seem problematic
+    hash = "sha256-FbbJPYotRHn85BV7p7wRSlAsPW1lSWrPCbEAgHq01QI="; # Reset this to get the correct hash for main branch
   };
 
-  cargoHash = "sha256-vNmySJ/bmSPpEHgIGTqs5Lf4GrhRfD6JdqVv+renUy8=";
+  cargoHash = "sha256-d9vFKYtAoWFuYK2BcGx5X0PNWG2ICJFSowacB/qG1nY="; # Will need to be updated with actual hash
 
-  nativeBuildInputs = [pkg-config];
-  buildInputs = [libxkbcommon];
+  nativeBuildInputs = [
+    libcosmicAppHook
+    just
+  ];
+
+  dontUseJustBuild = true;
+  dontUseJustCheck = true;
+
+  justFlags = [
+    "--set"
+    "prefix"
+    (placeholder "out")
+    "--set"
+    "bin-src"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-applet-minimon"
+  ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "A COSMIC applet for displaying CPU/Memory/Network/Disk/GPU usage in the Panel or Dock";
